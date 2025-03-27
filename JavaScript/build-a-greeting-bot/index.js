6cd4a91afc157e520b6de233a507a09e118f4929
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2025-03-26 20:35:10 -0500

    add js projects

---
+++ JavaScript/build-a-greeting-bot/index.js
@@ -0,0 +1,22 @@
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+let bot;
+let botLocation;
+bot = "teacherBot";
+botLocation = "the universe";
+console.log("Allow me to introduce myself.");
+let botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+let botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+bot = "professorBot";
+let nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+bot = "awesomeTeacherBot";
+let newNicknameGreeting =
+  "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+let favoriteSubject = "Computer Science";
+let favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
+console.log(favoriteSubjectSentence);
+console.log("Well, it was nice to talk to you. Have a nice day!");
---
+++ JavaScript/build-a-javascript-trivia-bot/index.js
@@ -0,0 +1,27 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Jack";
+let botLocation = "USA";
+let favoriteLanguage = "JavaScript";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact =
+  favoriteLanguage + " is a lightweight interpreted programming language.";
+console.log(codingFact);
+codingFact =
+  favoriteLanguage +
+  " was created by Brendan Eich in a mere 10 days in 1995 for Netscape, designed to add interactivity to web pages.";
+console.log(codingFact);
+codingFact =
+  "Despite the similar name, " +
+  favoriteLanguage +
+  " and Java are distinct languages. Java is a statically typed, object-oriented language, while " +
+  favoriteLanguage +
+  " is dynamically typed and versatile in both functional and object-oriented programming.";
+console.log(codingFact);
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    "."
+);
