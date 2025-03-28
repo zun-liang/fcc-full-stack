c1085e3b5e582a3d6482462f0021a65b0fd0f307
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2025-03-27 21:30:00 -0500

    add new javascript projects

---
+++ JavaScript/build-a-sentence-maker/index.js
@@ -0,0 +1,48 @@
+let adjective = "happy";
+let noun = "kitten";
+let verb = "play";
+let place = "home";
+let adjective2 = "safe";
+let noun2 = "dog";
+let firstStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " " +
+  "who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("First story: " + firstStory);
+adjective = "scary";
+noun = "dragon";
+verb = "fighting";
+place = "forest";
+adjective2 = "big";
+noun2 = "lizards";
+let secondStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " " +
+  "who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("Second story: " + secondStory);
---
+++ JavaScript/build-a-teacher-chatbot/index.js
@@ -0,0 +1,36 @@
+console.log("Hi there!");
+let botName = "teacherBot";
+let greeting = `My name is ${botName}.`;
+console.log(greeting);
+let subject = "JavaScript";
+let topic = "strings";
+let sentence = `Today, you will learn about ${topic} in ${subject}.`;
+console.log(sentence);
+let strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
+console.log(strLengthIntro);
+console.log(subject.length);
+console.log(
+  `Here is an example of using the length property on the word ${topic}.`
+);
+console.log(topic.length);
+console.log(
+  `Here is an example of accessing the first letter in the word ${subject}.`
+);
+console.log(subject[0]);
+console.log(
+  `Here is an example of accessing the second letter in the word ${subject}.`
+);
+console.log(subject[1]);
+console.log(
+  `Here is an example of accessing the last letter in the word ${subject}.`
+);
+let lastCharacter = subject[subject.length - 1];
+console.log(lastCharacter);
+let learningIsFunSentence = "Learning is fun.";
+console.log(
+  "Here are examples of finding the positions of substrings in the sentence."
+);
+console.log(learningIsFunSentence.indexOf("Learning"));
+console.log(learningIsFunSentence.indexOf("fun"));
+console.log(learningIsFunSentence.indexOf("learning"));
+console.log("I hope you enjoyed learning today.");
