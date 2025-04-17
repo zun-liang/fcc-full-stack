2bddfb054b494b6e479864d61cbcbe2ef481be1a
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2025-04-17 13:48:30 -0500

    update project

--- JavaScript/build-a-pyramid-generator/index.js
+++ JavaScript/build-a-pyramid-generator/index.js
@@ -1,10 +1,25 @@
 const pyramid = (s, n, facingDownwards) => {
   let lineLength = 1 + (n - 1) * 2;
-  let spaceHalf = (lineLength - 1) / 2;
   let pyramidStr = "";
-  for (let i = 0; i < n; i++) {
-    pyramidStr += s;
+  if (!facingDownwards) {
+    for (let i = 1; i <= lineLength; i += 2) {
+      if (i === 1) {
+        pyramidStr +=
+          "\n" + " ".repeat((lineLength - i) / 2) + s.repeat(i) + "\n";
+      } else {
+        pyramidStr += " ".repeat((lineLength - i) / 2) + s.repeat(i) + "\n";
+      }
+    }
+  } else {
+    for (let i = lineLength; i >= 1; i -= 2) {
+      if (i === lineLength) {
+        pyramidStr +=
+          "\n" + " ".repeat((lineLength - i) / 2) + s.repeat(i) + "\n";
+      } else {
+        pyramidStr += " ".repeat((lineLength - i) / 2) + s.repeat(i) + "\n";
+      }
+    }
   }
+
   return pyramidStr;
 };
-console.log(pyramid("o", 4, false));
