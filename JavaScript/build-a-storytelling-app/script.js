cd66d73cce62003ffbfef4d2b442cd52da8dd031
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2025-04-29 17:01:55 -0500

    update project

--- JavaScript/build-a-storytelling-app/script.js
+++ JavaScript/build-a-storytelling-app/script.js
@@ -7,6 +7,27 @@ const storyObj = {
   scary: {
     story:
       "In the dark woods, a group of friends stumbled upon an old, abandoned cabin. They enter the cabin and awaken something malevolent that had been dormant for centuries.",
+    borderColor: "#ee4b2b",
   },
-  borderColor: "#ee4b2b",
+  funny: {
+    story:
+      "During a camping trip, Mark decided to show off his culinary skills by cooking dinner over an open fire. However, his attempt caused him to burn the dinner as well as his eyebrows off.",
+    borderColor: "#f1be32",
+  },
+  adventure: {
+    story:
+      "Lost in the heart of the Amazon rain forest, Sarah and Jake stumbled upon an ancient temple. They braved deadly traps and encountered strange wildlife, all while deciphering cryptic clues left behind by a mysterious civilization.",
+    borderColor: "#acd157",
+  },
 };
+
+const displayStory = (genre) => {
+  if (storyObj.hasOwnProperty(genre)) {
+    resultParagraph.textContent = storyObj[genre].story;
+    storyContainer.style.borderColor = storyObj[genre].borderColor;
+  }
+};
+
+scaryStoryBtn.addEventListener("click", () => displayStory("scary"));
+funnyStoryBtn.addEventListener("click", () => displayStory("funny"));
+adventureStoryBtn.addEventListener("click", () => displayStory("adventure"));
