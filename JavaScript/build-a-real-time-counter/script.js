63a9bfeb5780bf1c2c8f4a3134fb3694175e6010
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2025-05-10 11:40:15 -0500

    update project

--- JavaScript/build-a-real-time-counter/script.js
+++ JavaScript/build-a-real-time-counter/script.js
@@ -1,6 +1,13 @@
 const textInput = document.getElementById("text-input");
 const charCount = document.getElementById("char-count");
 const getLength = () => {
-  console.log(textInput.ariaValueMax.length);
+  let inputLength = textInput.value.length;
+  if (inputLength <= 50) {
+    charCount.textContent = `Character Count: ${inputLength}/50`;
+  } else {
+    charCount.textContent = `Character Count: 50/50`;
+    charCount.style.color = "red";
+    textInput.value = textInput.value.slice(0, 50);
+  }
 };
 textInput.addEventListener("input", getLength);
